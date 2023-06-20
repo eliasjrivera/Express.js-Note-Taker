b7f1b751fbd5e19b3a6b1891ae4ac687d6aee7c4
Elias Rivera <eliasjohnrivera@gmail.com>
2023-06-19 20:03:34 -0700

    removed commented code

--- server.js
+++ server.js
@@ -86,7 +86,6 @@ app.post('/api/notes', (req, res) => {
         }
     });
 
-
 // note delete
 app.delete('/api/notes/:id', (req, res) => {
     // parse out list of notes from db.json
@@ -101,7 +100,7 @@ app.delete('/api/notes/:id', (req, res) => {
         return selected.id != noteId;
     });
 
-    // updates db.json and note list
+    // rewrites db.json and note list
     fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
     res.json(noteList);
 });
@@ -110,21 +109,3 @@ app.delete('/api/notes/:id', (req, res) => {
 app.listen(PORT, () =>
     console.log(`Server listening at http://localhost:${PORT}`)
 );
-
-
-
-//-----------------TODO---------------
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
